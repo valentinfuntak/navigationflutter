59faaf6942e200b2eb26927264a4fd205fd75a78
valentinfuntak <valentinfuntakk@gmail.com>
2024-12-09 12:50:08 +0100

    Navigacija

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,46 @@ class MyApp extends StatelessWidget {
       initialRoute: "/",
       routes: {
         "/": (context) => HomePage(),
-        "/second": (context) => SecondPAge()
+        "/second": (context) => SecondPage()
       },
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Home page")),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, "/second");
+          },
+          child: const Text("Go to second page"),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Second page")),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text("Back"),
+        ),
+      ),
     );
   }
 }
